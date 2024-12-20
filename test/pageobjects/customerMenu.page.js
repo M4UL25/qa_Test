5ff94eefb2d7b74a519325d625cfd7c6a619ef83
Maulana Sandi Samudera <sandi2501.ss@gmail.com>
2024-12-20 15:00:22 +0700

    new

--- test/pageobjects/customerMenu.page.js
+++ test/pageobjects/customerMenu.page.js
@@ -3,6 +3,11 @@ import {browser, $} from '@wdio/globals'
 class CustomerPage{
     get btnTransaction() {return $('button=Transaction')}
     get tableTransaction() {return $('a=Date-Time')}
+    get btnDeposit() {return $('button=Deposit')}
+    get titleDeposit() {return $('label*=Deposited')}
+    get btnWithdraw() {return $('button=Withdrawl')}
+    get titleWithdraw() {return $('label*=Withdrawn')}
+    get btnLogout() {return $('button=Logout')}
 }
 
 export default new CustomerPage()
--- test/specs/customerMenu.spec.js
+++ test/specs/customerMenu.spec.js
@@ -14,4 +14,32 @@ describe('CUSTOMER MENU', () => {
 
         await expect(customerPage.tableTransaction).toHaveText('Date-Time')
     })
+
+    it('Membuka fitur deposit pada menu customer', async () => {
+        await customerPage.btnDeposit.click()
+
+        await expect(customerPage.titleDeposit).toHaveText(
+            expect.stringContaining('Amount to be Deposited :')
+        )
+    })
+
+    it('Membuka fitur withdraw pada menu customer', async () => {
+        await customerPage.btnWithdraw.click()
+
+        await expect(customerPage.titleWithdraw).toHaveText(
+            expect.stringContaining('Amount to be Withdrawn :')
+        )
+    })
+
+    it('Keluar dan kembali lagi ke halaman account customer', async () => {
+        await customerPage.btnLogout.click()
+
+        await expect(browser).toHaveUrl('https://www.globalsqa.com/angularJs-protractor/BankingProject/#/customer')
+    })
+})
+
+describe('DEPOSITE', () => {
+    beforeEach( async () => {
+        customerPage.depositMenu()
+    })
 })
